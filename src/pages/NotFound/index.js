42781040303f62c02588a8b0588fbfa4e0bedc01
DmytroScimusGit <d.tatochenko@gmail.com>
2024-01-28 13:43:31 +0200

    add styles for 404 page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,9 +1,16 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
 const NotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
-    </div>
+    <Container className="text-center mt-5">
+      <Row>
+        <Col>
+          <h1>404</h1>
+          <p>Page not found</p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
